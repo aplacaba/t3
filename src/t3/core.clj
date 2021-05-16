23b72b84000520c6627af3d44a017c1fa88b65c4
aplacaba <alacaba@fastmail.com>
2021-05-16 15:32:57 +0800

    Added tests for check winner

--- src/t3/core.clj
+++ src/t3/core.clj
@@ -8,19 +8,29 @@
         tup (fn [n] [(keyword (str "s" n)) n])]
     (into {} (map tup squares))))
 
+(def default-state
+  {:board game-board
+   :current_player "x"
+   :status :stopped
+   :history []
+   :scores {:x 0
+            :y 0
+            :draw 0}})
 
-;; state
-(def game-state
-  "Game state holder"
-  (atom {:board game-board
-         :current_player "x"
-         :history []}))
+
+(def game-state (atom default-state))
 
 (defn reset-state
   []
-  (swap! game-state (fn [_] {:board game-board
-                            :current_player "x"
-                            :history []})))
+  (swap! game-state (fn [_] default-state)))
+
+(defn reset-board
+  []
+  (swap! game-state assoc :board game-board))
+
+(defn reset-scores
+  []
+  (swap! game-state assoc :scores {:x 0 :y 0 :draw 0}))
 
 (defn next-player
   []
@@ -37,28 +47,32 @@
   [board k sym]
   (assoc board k sym))
 
-
 (defn check-winner
   "Check winner from the gameboard"
   [gb]
-  (and
-   ;; check if gameboard contains enough pieces
-   (>= (count
-        (filter (fn [s] (not (int? s)))
-                (vals gb)))
-       5)
-   (or
-    ;; horizontals
-    (= (:s0 gb) (:s1 gb) (:s2 gb))
-    (= (:s3 gb) (:s4 gb) (:s5 gb))
-    (= (:s6 gb) (:s7 gb) (:s8 gb))
-    ;; verticals
-    (= (:s0 gb) (:s3 gb) (:s6 gb))
-    (= (:s1 gb) (:s4 gb) (:s7 gb))
-    (= (:s2 gb) (:s5 gb) (:s8 gb))
-    ;; diagonals
-    (= (:s0 gb) (:s4 gb) (:s8 gb))
-    (= (:s2 gb) (:s4 gb) (:s6 gb)))))
+  (let [pc-count (count (filter (fn [s] (not (int? s))) (vals gb)))]
+    (cond
+      (and
+       ;; check if gameboard contains enough pieces
+       (>= pc-count 5)
+       (or
+        ;; horizontals
+        (= (:s0 gb) (:s1 gb) (:s2 gb))
+        (= (:s3 gb) (:s4 gb) (:s5 gb))
+        (= (:s6 gb) (:s7 gb) (:s8 gb))
+        ;; verticals
+        (= (:s0 gb) (:s3 gb) (:s6 gb))
+        (= (:s1 gb) (:s4 gb) (:s7 gb))
+        (= (:s2 gb) (:s5 gb) (:s8 gb))
+        ;; diagonals
+        (= (:s0 gb) (:s4 gb) (:s8 gb))
+        (= (:s2 gb) (:s4 gb) (:s6 gb))))
+      :win
+
+      (>= pc-count 9)
+      :draw
+
+      :else :noresult)))
 
 
 (defn draw-board
@@ -114,13 +128,24 @@
   (if (validator x)
     x
     (let []
-      (println "Invalid input: ")
+      (println "Invalid input\n")
       (ask-input text validator))))
+
+(defn game-is-started? []
+  (= :started (:status @game-state)))
+
+(defn game-setup
+  []
+  (swap! game-state (fn [state] merge(state {:board game-board
+                                             :status :started}))))
 
 (defn game-loop
   []
+  (game-setup)
+  (while (game-is-started?)
+    )
   (while (not (check-winner (:board @game-state)))
-    (println "Tic Tac Toe")
+    (println "Tic Tac Toe\n\n\n")
     (draw-board (:board @game-state))
     (println "Current Player: " (:current_player @game-state))
     (def move (ask-input "Enter square number: " move-validator))
@@ -133,7 +158,5 @@
   (println (:current_player @game-state) " wins")
   (def play-again? (ask-input "Play again? (y/n)" yn-validator))
   (if (= "y" play-again?)
-    (fn []
-      (reset-state)
-      (game-loop))
+    game-loop
     (println "Thanks for playing.")))
--- test/t3/core_test.clj
+++ test/t3/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [t3.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest check-winner-test
+  (testing "draw"
+    (let [b {:s0 "x" :s1 "o" :s2 "x"
+             :s3 "o" :s4 "x" :s5 "o"
+             :s6 "o" :s7 "x" :s8 "o"}]
+      (is (= :draw (check-winner b)))))
+
+  (testing "win"
+    (let [b (merge game-board {:s0 "x" :s1 "x" :s2 "x"
+                               :s3 "o" :s4 "o"})]
+      (is (= :win (check-winner b)))))
+
+  (testing "noresult"
+    (is (= :noresult (check-winner game-board)))))
